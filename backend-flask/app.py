67fab868112c7c6662481ca3ba7a3f2e5b52ba12
ashish dixit <dixitashish6@gmail.com>
2024-11-11 15:46:43 +0000

    added instrumentation for Honeycomb

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -16,12 +16,7 @@ from services.show_activity import *
 
 
 #### HoneyComb #####
-from opentelemetry import trace
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
 
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
