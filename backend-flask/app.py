0104c758cd7ca9422c9f07b1a1765607f8478ba7
ashish dixit <dixitashish6@gmail.com>
2024-11-12 20:27:46 +0000

    fixing rollbar instrumentation

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -94,26 +94,19 @@ cors = CORS(
 
 rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
 
-first_request = True
-@app.before_request
-def before_first_request():
-    global first_request
-    if first_request:
-        def init_rollbar():
-            """init rollbar module"""
-            rollbar.init(
-                # access token
-                rollbar_access_token,
-                # environment name
-                'production',
-                # server root directory, makes tracebacks prettier
-                root=os.path.dirname(os.path.realpath(__file__)),
-                # flask already sets up logging
-                allow_logging_basic_config=False)
-
-            # send exceptions from `app` to rollbar, using flask's signal system.
-            got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
-            first_request = False
+with app.app_context():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name - any string, like 'production' or 'development'
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)        
 
 # Rollbar ---
 @app.route('/rollbar/test')
