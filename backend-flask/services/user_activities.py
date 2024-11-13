37d0fd7da3755eb374424887d638dba91bc18a45
ashish dixit <dixitashish6@gmail.com>
2024-11-13 16:05:28 +0000

    instrument xray subsegment

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -4,14 +4,13 @@ class UserActivities:
   def run(user_handle):
     # xray ----
     segment = xray_recorder.begin_segment('user_activities')
+
     model = {
       'errors': None,
       'data': None
     }
 
     now = datetime.now(timezone.utc).astimezone()
-
-
 
     if user_handle == None or len(user_handle) < 1:
       model['errors'] = ['blank_user_handle']
@@ -29,9 +28,8 @@ class UserActivities:
     subsegment = xray_recorder.begin_subsegment('mock-data')
   
     dict = {
-      "now": now.isformat(),
+      "now": datetime.now(timezone.utc).astimezone(),
       "results-size": len(model['data'])
     }
     subsegment.put_metadata('key', dict, 'namespace')
-
     return model
