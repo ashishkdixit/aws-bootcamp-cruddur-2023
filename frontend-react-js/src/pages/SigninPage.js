a4fcc580e5b5acbb350cfb1f5104461541274bc2
ashish dixit <dixitashish6@gmail.com>
2024-11-14 19:55:39 +0000

    add code for signin js

--- frontend-react-js/src/pages/SigninPage.js
+++ frontend-react-js/src/pages/SigninPage.js
@@ -1,10 +1,9 @@
 import './SigninPage.css';
 import React from "react";
-import {ReactComponent as Logo} from '../components/svg/logo.svg';
+import { ReactComponent as Logo } from '../components/svg/logo.svg';
 import { Link } from "react-router-dom";
-
-// [TODO] Authenication
-import Cookies from 'js-cookie'
+// Importing only the signIn method from @aws-amplify/auth
+import { signIn } from '@aws-amplify/auth';
 
 export default function SigninPage() {
 
@@ -13,27 +12,33 @@ export default function SigninPage() {
   const [errors, setErrors] = React.useState('');
 
   const onsubmit = async (event) => {
+    setErrors('');
     event.preventDefault();
-    setErrors('')
-    console.log('onsubmit')
-    if (Cookies.get('user.email') === email && Cookies.get('user.password') === password){
-      Cookies.set('user.logged_in', true)
-      window.location.href = "/"
-    } else {
-      setErrors("Email and password is incorrect or account doesn't exist")
+
+    try {
+      const user = await signIn(email, password); // Use signIn directly
+      localStorage.setItem("access_token", user.signInUserSession.accessToken.jwtToken);
+      window.location.href = "/";
+    } catch (error) {
+      if (error.code === 'UserNotConfirmedException') {
+        window.location.href = "/confirm";
+      } else {
+        setErrors(error.message);
+      }
     }
-    return false
-  }
+    return false;
+  };
 
   const email_onchange = (event) => {
     setEmail(event.target.value);
-  }
+  };
+
   const password_onchange = (event) => {
     setPassword(event.target.value);
-  }
+  };
 
   let el_errors;
-  if (errors){
+  if (errors) {
     el_errors = <div className='errors'>{errors}</div>;
   }
 
@@ -43,10 +48,7 @@ export default function SigninPage() {
         <Logo className='logo' />
       </div>
       <div className='signin-wrapper'>
-        <form 
-          className='signin_form'
-          onSubmit={onsubmit}
-        >
+        <form className='signin_form' onSubmit={onsubmit}>
           <h2>Sign into your Cruddur account</h2>
           <div className='fields'>
             <div className='field text_field username'>
@@ -71,16 +73,12 @@ export default function SigninPage() {
             <Link to="/forgot" className="forgot-link">Forgot Password?</Link>
             <button type='submit'>Sign In</button>
           </div>
-
         </form>
         <div className="dont-have-an-account">
-          <span>
-            Don't have an account?
-          </span>
+          <span>Don't have an account?</span>
           <Link to="/signup">Sign up!</Link>
         </div>
       </div>
-
     </article>
   );
 }
