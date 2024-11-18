f524da082a74f752e79d8e154a237623c4327f6b
ashish dixit <dixitashish6@gmail.com>
2024-11-18 20:30:40 +0000

    code for password recovery

--- frontend-react-js/src/pages/RecoverPage.js
+++ frontend-react-js/src/pages/RecoverPage.js
@@ -2,6 +2,8 @@ import './RecoverPage.css';
 import React from "react";
 import {ReactComponent as Logo} from '../components/svg/logo.svg';
 import { Link } from "react-router-dom";
+import { resetPassword,confirmResetPassword } from '@aws-amplify/auth';
+
 
 export default function RecoverPage() {
   // Username is Eamil
@@ -14,12 +16,25 @@ export default function RecoverPage() {
 
   const onsubmit_send_code = async (event) => {
     event.preventDefault();
-    console.log('onsubmit_send_code')
+    setErrors('')
+    await resetPassword({username: username})
+    .then((data) => setFormState('confirm_code') )
+    .catch((err) => setErrors(err.message) );
     return false
   }
+
+
   const onsubmit_confirm_code = async (event) => {
     event.preventDefault();
-    console.log('onsubmit_confirm_code')
+    if (password == passwordAgain){
+      confirmResetPassword({username: username,
+        confirmationCode:code, 
+        newPassword: password})
+      .then((data) => setFormState('success'))
+      .catch((err) => setErrors(err.message) );
+    } else {
+      setErrors('Passwords do not match')
+    }
     return false
   }
 
@@ -78,6 +93,14 @@ export default function RecoverPage() {
           <input
             type="text"
             value={code}
+            onChange={code_onchange} 
+          />
+        </div>
+        <div className='field text_field code'>
+          <label>Email</label>
+          <input
+            type="text"
+            value={username}
             onChange={code_onchange} 
           />
         </div>
