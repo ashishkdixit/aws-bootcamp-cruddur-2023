0f3b345731d67c6c62bb2d9537da69cd68c44468
ashish dixit <dixitashish6@gmail.com>
2024-11-22 03:03:08 +0000

    display data in the ui from local db

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,12 +3,13 @@ INSERT INTO public.users (display_name, handle, cognito_user_id)
 VALUES
   ('Andrew Brown', 'andrewbrown' ,'MOCK'),
   ('Andrew Bayko', 'bayko' ,'MOCK'),
-  ('Luffy Sencho', 'thecat' ,'MOCK');
+  ('Luffy Sencho', 'thecat' ,'MOCK'),
+  ('Jane Doe', 'jane' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'thecat' LIMIT 3),
+    (SELECT uuid from public.users WHERE users.handle = 'jane' LIMIT 4),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
